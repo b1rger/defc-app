6a10d9ca3f1a85fb229768ec76e9a9d0b126ce52
Birger Schacht <birger@rantanplan.org>
2023-05-04 13:08:01 +0200

    Replace MIDDLEWARE_CLASSES with MIDDLEWARE
    and comment the SessionAuthenticationMiddleware (its apparently gone)

--- orea/settings/base.py
+++ orea/settings/base.py
@@ -78,17 +78,17 @@ HAYSTACK_CONNECTIONS = {
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'reversion.middleware.RevisionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-)
+    ]
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 
