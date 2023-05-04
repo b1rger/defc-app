7c85baab705cbdc469d9b17d8b2e2b141d50924d
Birger Schacht <birger@rantanplan.org>
2023-05-04 12:52:57 +0200

    Replace render_to_response with render

--- browsing/views.py
+++ browsing/views.py
@@ -15,7 +15,6 @@ import datetime
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
